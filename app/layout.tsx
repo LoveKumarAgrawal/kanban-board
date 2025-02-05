c18e62667def65f434292dc0516d009deffb3599
LoveKumarAgrawal <loveagrawal204@gmail.com>
2025-02-05 14:36:55 +0530

    added toaster context

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import ToasterContext from "@/context/ToasterContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +30,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="flex flex-col h-screen">
-      <Navbar />
-      {children}
-      </div>
+          <ToasterContext />
+          <Navbar />
+          {children}
+        </div>
       </body>
     </html>
   );
